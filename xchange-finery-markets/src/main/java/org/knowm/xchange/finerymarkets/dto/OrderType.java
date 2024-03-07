ebcd09d536701659e5842300c35695970432aa94
David Zsombok <david.zsombok@baxter-it.com>
2024-03-07 21:57:55 +0100

    DL-927 Add javadoc to abbreviated enums

--- xchange-finery-markets/src/main/java/org/knowm/xchange/finerymarkets/dto/OrderType.java
+++ xchange-finery-markets/src/main/java/org/knowm/xchange/finerymarkets/dto/OrderType.java
@@ -3,9 +3,13 @@ package org.knowm.xchange.finerymarkets.dto;
 public enum OrderType {
   LIMIT(0),
   POST_ONLY(1),
+  /** Immediate-Or-Cancel */
   LIMIT_IOC(2),
+  /** Fill-Or-Kill */
   LIMIT_FOK(3),
+  /** Immediate-Or-Cancel */
   MARKET_IOC(4),
+  /** Fill-Or-Kill */
   MARKET_FOK(5),
   MANUAL_TRADE(6),
   LIQUIDATION_TRADE(7);
