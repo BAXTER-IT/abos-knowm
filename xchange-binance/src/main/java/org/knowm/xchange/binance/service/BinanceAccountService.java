262ab47f47b72905c6145a1375ef0bf6b29f11e1
Dmitri Karpovich <dmitri.karpovich@gmail.com>
2025-08-20 23:55:06 +0200

    Change signature of getDynamicTradingFeesByInstrument

--- xchange-binance/src/main/java/org/knowm/xchange/binance/service/BinanceAccountService.java
+++ xchange-binance/src/main/java/org/knowm/xchange/binance/service/BinanceAccountService.java
@@ -144,7 +144,8 @@ public class BinanceAccountService extends BinanceAccountServiceRaw implements A
 
   /** Results based on ExchangeSpecificParametersItem(EXCHANGE_TYPE) */
   @Override
-  public Map<Instrument, Fee> getDynamicTradingFeesByInstrument() throws IOException {
+  public Map<Instrument, Fee> getDynamicTradingFeesByInstrument(String... category)
+      throws IOException {
     try {
       Map<Instrument, Fee> fees = new HashMap<>();
       if (exchange
