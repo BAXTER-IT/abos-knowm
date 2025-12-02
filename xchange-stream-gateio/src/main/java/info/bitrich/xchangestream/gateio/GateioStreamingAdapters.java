327993934a8b5ca6e2314c06d54eb6c135df2b3b
David Zsombok <zsombokd@gmail.com>
2025-12-02 11:56:24 +0100

    Fix balance mappings

--- xchange-stream-gateio/src/main/java/info/bitrich/xchangestream/gateio/GateioStreamingAdapters.java
+++ xchange-stream-gateio/src/main/java/info/bitrich/xchangestream/gateio/GateioStreamingAdapters.java
@@ -162,7 +162,7 @@ public class GateioStreamingAdapters {
         .amount(amount)
         .type(type)
         .status(Status.COMPLETE)
-        .balance(in.getTotal())
+        .balance(in.getAvailable())
         .fee(fee)
         .description(buildSpotDescription(in))
         .build();
@@ -181,7 +181,6 @@ public class GateioStreamingAdapters {
     return FundingRecord.builder()
         .date(extractDate(in))
         .currency(Currency.getInstance(in.getText().split("_")[0]))
-        .amount(in.getBalance().abs())
         .type(type)
         .status(Status.COMPLETE)
         .balance(in.getBalance())
